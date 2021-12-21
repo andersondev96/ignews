3057e73611371169fdba758a3953e335a58fe735
Anderson Fernandes <andersonfferreira13@hotmail.com>
2021-12-21 11:43:35 -0300

    Commit to deploy

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,44 @@
+import { signIn, useSession } from "next-auth/client";
+import styles from "./styles.module.scss";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import { useRouter } from "next/router";
+
+export function SubscribeButton() {
+  const [session] = useSession();
+  const router = useRouter();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
+}
